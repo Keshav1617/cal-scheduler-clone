3f84b8e9f527709fadc507156eee2b91572eb5cc
Keshav Jha <keshavjha1627@gmail.com>
2026-03-13 23:03:07 +0530

    fix typescript react types

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,11 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || '*',
+    origin: [
+      process.env.CLIENT_URL,
+      "http://localhost:5173"
+    ],
+    credentials: true
   })
 );
 app.use(express.json());
